dd5cfe4386f6eb15aa81d4f84c802c5afec8a3d4
Bryan Horna <bryanjhv@gmail.com>
2017-10-21 17:54:45 -0500

    Add TypeScript declaration

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="chai" />
+
+declare global {
+    namespace Chai {
+        interface Assertion {
+            excluding(props: string|string[]): Assertion;
+        }
+    }
+}
+
+declare function chaiExclude(chai: any, utils: any): void;
+export = chaiExclude;
