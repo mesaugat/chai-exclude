f44724f8121643ae565f037f84ade2d796ba658c
Saugat Acharya <mesaugat@gmail.com>
2018-02-05 22:12:14 +0545

    Add declaration for deepEqualExcluding and deepEqualExcludingEvery. closes #11

--- lib/chai-exclude.d.ts
+++ lib/chai-exclude.d.ts
@@ -11,4 +11,28 @@ declare namespace Chai {
     excluding(props: string | string[]): Assertion;
     excludingEvery(props: string | string[]): Assertion;
   }
+
+  interface Assert {
+    /**
+     * Asserts that actual is deeply equal to expected excluding some top level properties.
+     *
+     * @type T          Type of the objects.
+     * @param actual    Actual value.
+     * @param expected  Potential expected value.
+     * @param props     Properties or keys to exclude.
+     * @param message   Message to display on error.
+     */
+    deepEqualExcluding<T>(actual: T, expected: T, props: string | string[], message?: string): void;
+
+    /**
+     * Asserts that actual is deeply equal to expected excluding properties any level deep.
+     *
+     * @type T          Type of the objects.
+     * @param actual    Actual value.
+     * @param expected  Potential expected value.
+     * @param props     Properties or keys to exclude.
+     * @param message   Message to display on error.
+     */
+    deepEqualExcludingEvery<T>(actual: T, expected: T, props: string | string[], message?: string): void;
+  }
 }
